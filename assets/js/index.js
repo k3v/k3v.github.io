b6b923a630a0d284ebfd5463dc36fba15ffb5771
furryfaust <kevtehpro35@gmail.com>
2017-06-28 00:37:19 -0400

    Added content transitions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,56 @@
-window.onload = function () {
+var open;
+var vanish;
+function getSection() {
+    let url = new URL(window.location.href);
+    return url.pathname.split("/")[1];
+}
 
-    var open;
-    function getSection() {
-        let url = new URL(window.location.href);
-        return url.pathname.split("/")[1];
+function toggle(next, curr) {
+    clearInterval(open);
+    clearInterval(vanish);
+
+    let delay = 1000;
+    if (curr != "") {
+        let currEl = document.getElementById(curr);
+        currEl.classList.add("inactive");
+
+        let currBtn = document.getElementById(curr + "-button");
+        currBtn.classList.remove("active");
+
+        vanish = setTimeout(function () {
+            currEl.classList.add("off");
+        }, delay);
+    } else {
+        delay = 0;
     }
 
-    function toggle(next, curr) {
-        clearInterval(open);
+    if (next != "") {
+        let nextBtn = document.getElementById(next + "-button");
+        nextBtn.classList.add("active");
 
-        let delay = 1200;
-        if (curr != "") {
-            let currEl = document.getElementById(curr);
-            currEl.classList.add("inactive");
+        open = setTimeout(function () {
+            let nextEl = document.getElementById(next);
+            nextEl.classList.remove("inactive");
+            nextEl.classList.remove("off");
+        }, delay);
+    }
+
+    window.history.replaceState(null, null, next == "" ? "/" : "/" + next + "/");
+}
+
+function generateHandler(section) {
+    return function () {
+        if (getSection() == section) {
+            toggle("", section);
         } else {
-            delay = 0;
+            toggle(section, getSection());
         }
+    };
+}
 
-        if (next != "") {
-            open = setTimeout(function () {
-                let nextEl = document.getElementById(next);
-                nextEl.classList.remove("inactive");
-            }, delay);
-        }
-
-        window.history.replaceState(null, null, next + "/");
-    }
-
-    function generateHandler(section) {
-        return function () {
-            if (getSection() == section) {
-                toggle("", section);
-            } else {
-                toggle(section, getSection());
-            }
-        };
-    }
-
-    var sections = ["greet"];
+window.onload = function () {
+    var sections = ["greet", "thoughts", "notes", "projects"];
     for (let section of sections) {
         document.getElementById(section + "-button").onclick = generateHandler(section);
     }
-
 };
