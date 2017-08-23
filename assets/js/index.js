2da662ba94c0cb496494e6996d1985e20db5c375
Kevin Zheng <kevtehpro35@gmail.com>
2017-08-23 00:14:47 -0400

    Added WIP content framework and tinkered w/ colors

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,7 +22,7 @@ function toggle(next, curr) {
         }, delay);
     } else {
         delay = 0;
-    }
+    } 
 
     if (next != "") {
         let nextBtn = document.getElementById(next + "-button");
@@ -48,9 +48,47 @@ function generateHandler(section) {
     };
 }
 
-window.onload = function () {
-    var sections = ["greet", "thoughts", "notes", "projects"];
-    for (let section of sections) {
-        document.getElementById(section + "-button").onclick = generateHandler(section);
+function fetchThoughts() {
+    if (getSection() != "thoughts") {
+        return;
     }
-};
+
+    let request = new XMLHttpRequest();
+    request.addEventListener("load", populateThoughts);
+    request.open("GET", "/thoughts/thoughts.json");
+    request.send(); 
+}
+
+function populateThoughts() {
+    let thoughts = JSON.parse(this.responseText);
+    let container = document.getElementById("thoughts");
+
+/*
+    container.innerHTML = "";
+
+    for (let thought of thoughts) {
+        let tEl = document.createElement("div");
+        tEl.classList.add("thought");
+
+        let title = document.createElement("div");
+        title.classList.add("title");
+        let ttext = document.createElement("p");
+        ttext.innerText = thought.title;
+        title.appendChild(ttext);
+        tEl.appendChild(title);
+
+        
+
+        container.appendChild(tEl);
+    }
+*/
+}
+
+window.addEventListener("load", function () {
+    let sections = ["greet", "thoughts", "notes", "projects"];
+    for (let section of sections) {
+        document.getElementById(section + "-button").addEventListener("click", generateHandler(section));
+    }
+
+    document.getElementById("thoughts-button").addEventListener("click", fetchThoughts);
+});
